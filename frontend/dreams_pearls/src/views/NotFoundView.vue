6cf643a2053e843db2cba99195deb22cb739f3c4
miraclekp25 <beniemiracle06@gmail.com>
2025-05-24 13:07:43 +0100

    save

--- frontend/dreams_pearls/src/views/NotFoundView.vue
+++ frontend/dreams_pearls/src/views/NotFoundView.vue
@@ -1,12 +1,12 @@
 <script setup>
-import HeaderComponent from '../components/HeaderComponent.vue'
 </script>
 
 <template>
-    <HeaderComponent/>
 
-    <h1 class="text-7xl mx-auto my-50 text-center font-bold font-serif text-gray-200">404 Not found</h1>
-
+    <div class="flex flex-col my-25 items-center gap-7">
+      <h1 class="text-7xl mx-auto text-center font-bold font-serif text-gray-200">404 Not found</h1>
+      <router-link to="/" class="bg-indigo-600 p-3 w-50 text-white text-center font-medium font-serif hover:bg-indigo-800 rounded-lg">&LeftArrow; Retour page d'accueil</router-link>
+    </div>
 
 </template>
 
